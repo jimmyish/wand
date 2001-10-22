2c4df6070b23f34f6959aacea0583a9400f9b198
Perry Lorier <github@isomer.meta.net.nz>
2001-10-22 12:00:33 +0000

    Fixed compile problem with testing

--- Ethernet-Over-UDP/src/list.cc
+++ Ethernet-Over-UDP/src/list.cc
@@ -54,8 +54,8 @@ int main(int argc,char **argv)
 	assert(rem_ip(ether)==false);
 	add_ip(ether,ip_t(0x04030201));
 	add_ip(ether2,ip_t(0x04030202));
-	assert(find_ip(ether) == ip_t(0x04030201));
 	assert(find_ip(ether2) == ip_t(0x04030202));
+	assert(find_ip(ether) == ip_t(0x04030201));
 	assert(rem_ip(ether)!=false);
 	assert(rem_ip(ether2)!=false);
 }
