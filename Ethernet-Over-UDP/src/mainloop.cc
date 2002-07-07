5855b57815b06110b881f8231c8c2bac0005dad6
Jamie Curtis <jamie@pointless.co.nz>
2002-07-07 04:31:22 +0000

    Perry's hacks

--- Ethernet-Over-UDP/src/mainloop.cc
+++ Ethernet-Over-UDP/src/mainloop.cc
@@ -67,7 +67,7 @@ void mainloop(void)
 	}
 	while(1) {
 		rfd2=rfd;
-		select(highestfd+1,&rfd2,NULL,NULL,NULL);
+		select(highestfd+2,&rfd2,NULL,NULL,NULL);
 		for (fd2callback_t::const_iterator i=fd2callback.begin();
 		     i!=fd2callback.end();
 		     i++)
