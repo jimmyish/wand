eb60d9b536db36530ec798f6f56572a49847e572
Jamie Curtis <jamie@pointless.co.nz>
2002-07-07 04:48:18 +0000

    Lotsa init debugging

--- Ethernet-Over-UDP/src/Etud.cc
+++ Ethernet-Over-UDP/src/Etud.cc
@@ -43,30 +43,41 @@ int main(int arvc,char **argv)
 		{ "daemonise", TYPE_BOOL|TYPE_NULL, &do_daemonise },
 		{ NULL, 0, NULL }
 	};
+	
+	logger(MOD_INIT, 15, "About to parse config\n");
 	if (parse_config(main_config,"/usr/local/etc/wand.conf")) {
 	  logger(MOD_INIT,1,"Bad Config file, giving up\n");
 	  return 1;
 	}
+	
+	logger(MOD_INIT, 15, "Parsed config, about to load driver\n");
 	if (!load_module(module)) {
 		logger(MOD_INIT, 1, "Aborting...\n");
 		return 1;
 	}
+	
+	logger(MOD_INIT, 15, "Loaded driver, about to init interface\n");
 	if (!init_interface()) {
 		logger(MOD_INIT, 1, "Failed to initialise interface.\n");
 		logger(MOD_INIT, 1, "Aborting...\n");
 		return 1;
 	}
+
+	logger(MOD_INIT, 15, "Initialised interface, about to start UDP\n");
 	if (udp_start()<0) {
 		logger(MOD_INIT, 1, "Failed to create udp socket.\n");
 		logger(MOD_INIT, 1, "Aborting...\n");
 		//device->down();
 		return 1;
 	}
+
+	logger(MOD_INIT, 15, "UDP started, about to start UNIX domain socket\n");
 	if (ui_setup()<0) {
 		logger(MOD_INIT, 1, "Failed to create unix domain socket.\n");
 		//interface->down();
 		return 1;
 	}
+
 	logger(MOD_INIT, 6, "Etud started\n");
 	logger(MOD_INIT, 6, "Using interface driver: %s\n", driver->name);
 	logger(MOD_INIT, 6, " version: %s\n", driver->version);
