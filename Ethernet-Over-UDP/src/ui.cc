016049c3e990e787e6628c5c1a3c82c52dc3372d
Matt Brown <matt@mattb.net.nz>
2003-03-06 11:43:01 +0000

    Removed an erronous printf!

--- Ethernet-Over-UDP/src/ui.cc
+++ Ethernet-Over-UDP/src/ui.cc
@@ -90,7 +90,6 @@ static void m_getmac(int fd,char **argv,int argc)
 {
 	char tbuff[80];
 	sprintf(tbuff,"+GETMAC %s\r\n", macaddr);
-	printf("+GETMAC %s\r\n", macaddr);
 	ui_send(fd, tbuff);
 	ui_send(fd, "-OK\r\n");
 	printf("-OK\r\n");
